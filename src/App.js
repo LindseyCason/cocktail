92d61b49d13c219b8a74d06a186fbca91ce2bd08
Lindsey Cason <lindseyacason@gmail.com>
2021-03-02 16:57:54 -0600

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from "./logo.svg";
+import "./App.css";
+import { Cocktails } from "./components/Cocktails";
+import axios from "axios";
+
+function App() {
+  return (
+    <div>
+      <Cocktails />
+    </div>
+  );
+}
+
+export default App;
